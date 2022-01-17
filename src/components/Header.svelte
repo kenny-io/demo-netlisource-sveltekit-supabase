c6b104616eb6d350a4b60cdc6183da787c5465c9
kenny-io <ekeneeze3@gmail.com>
2022-01-18 01:05:05 +0400

    add supabase and some css

---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<div class="header">
+    <img class="logo" src="/logo.png" alt="logo" />
+    <h2 class="logotext">Source</h2>
+</div>
+
+<style>
+    .header {
+        background-color: #E9EAEB;
+        height: 120px;
+        display: flex;
+    }
+    .logo {
+        width: 148px;
+        height: 40px;
+        position: absolute;
+        top: 40px;
+        left: 301px;
+    }
+    .logotext {
+        position: absolute;
+        top: 45px;
+        left: 449px;
+    }
+</style>
